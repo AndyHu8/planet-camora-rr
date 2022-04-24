8ee06834e64f32bf04ebcfcccad37eaa595b3893
Andy Hu <huandy01@gmail.com>
2022-04-24 21:28:56 +0200

    Bilder

---
+++ planet/src/Navigation/Navigation.js
@@ -0,0 +1,20 @@
+export default function Navigation() {
+    return (
+        <div className="container">
+            <div className="socialIcons">
+                <ul>
+                    <li><img src="./PlanetCamoraBilder/twitter.png"/></li>
+                    <li><img/></li>
+                    <li><img/></li>
+                </ul>
+            </div>
+            <div className="socialTexts">
+                <ul>
+                    <li>Kaufen</li>
+                    <li>Startseite</li>
+                    <li>Vorteile</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
