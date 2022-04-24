13d6297a9440e17ee311ef4df94dc5823b695f3c
Andy Hu <huandy01@gmail.com>
2022-04-24 22:39:09 +0200

    Navi fertig, nicht RPV

--- planet/src/Navigation/Navigation.js
+++ planet/src/Navigation/Navigation.js
@@ -1,17 +1,24 @@
+import "./Navigation.css"
+import twitter from "../PlanetCamoraBilder/twitter.png"
+import insta from "../PlanetCamoraBilder/insta.png"
+import discord from "../PlanetCamoraBilder/dc.png"
+import openSea from "../PlanetCamoraBilder/opensea.png"
+
 export default function Navigation() {
     return (
         <div className="container">
-            <div className="socialIcons">
-                <ul>
-                    <li><img src="./PlanetCamoraBilder/twitter.png"/></li>
-                    <li><img/></li>
-                    <li><img/></li>
+            <div>
+                <ul className="socialIcons">
+                    <li><img src={twitter} onClick={() => window.open("https://twitter.com/PlanetCamora", "_blank")}/></li>
+                    <li><img src={insta} onClick={() => window.open("https://www.instagram.com/planetcamoranft/", "_blank")}/></li>
+                    <li><img src={discord} onClick={() => window.open("https://discord.com/invite/planetcamora", "_blank")}/></li>
+                    <li><img src={openSea}/></li>
                 </ul>
             </div>
-            <div className="socialTexts">
-                <ul>
+            <div>
+                <ul className="socialTexts">
                     <li>Kaufen</li>
-                    <li>Startseite</li>
+                    <li onClick={() => window.location.reload()}>Startseite</li>
                     <li>Vorteile</li>
                 </ul>
             </div>
