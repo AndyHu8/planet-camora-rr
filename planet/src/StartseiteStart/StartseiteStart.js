f7d0faf7f6e703cf4c6cf09b5e62a98ce4d02ee4
Andy Hu <huandy01@gmail.com>
2022-04-25 21:37:21 +0200

    Home fertig

---
+++ planet/src/StartseiteStart/StartseiteStart.js
@@ -0,0 +1,21 @@
+import PlanetCamoraSchrift from "../PlanetCamoraBilder/schrift.png"
+import HauptseiteBanner from "../PlanetCamoraBilder/hauptseiteBanner.png"
+import "./StartseiteStart.css"
+
+
+export default function StartseiteStart() {
+    return (
+        <section>
+            <div className="startseiteContainer">
+                <div className="boxLinks">
+                    <img src={PlanetCamoraSchrift}></img>
+                    <p>Raf Camoras Album ZUKUNFT feiert den Gold Erfolg! Zum Dank an seine Fans, möchte Raf Camora mit dem Projekt PLANET CAMORA etwas zurückgeben. Erhalte jetzt einen zertifizierten Teil der Goldplatte des Raf Camora Albums Zukunft. Erhalte Zugriff zum ersten NFT Song release im deutschsprachigen Raum, sowie lifetime VIP Konzerttickets, exklusive Meet and Greets, Inner Circle Memberships und vieles mehr.</p>
+                    <div className="button">Was ist ein NFT?</div>
+                </div>
+                <div className="boxRechts">
+                    <img src={HauptseiteBanner}></img>
+                </div>
+            </div>
+        </section>
+    )
+}
--- planet/src/index.js
+++ planet/src/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import Navigation from './Navigation/Navigation';
+import StartseiteStart from './StartseiteStart/StartseiteStart';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Navigation/>
+    <StartseiteStart/>
   </React.StrictMode>
 );
