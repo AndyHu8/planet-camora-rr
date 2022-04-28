3087c32bb586345e3cd04de3067eb1167f6ae568
Andy Hu <huandy01@gmail.com>
2022-04-28 22:48:10 +0200

    WasNft fertig

---
+++ planet/src/WasNft/WasNft.js
@@ -0,0 +1,14 @@
+import "./WasNft.css"
+
+export default function WasNft() {
+    return (
+        <section className="WasNftSection">
+            <div>
+                <h2>Was ist ein <span>NFT</span>?</h2>
+                <p>NFT steht für nicht fungible Token. NFTs sind digitale Unikate, welche nicht ersetzt werden können. Diese können so ziemlich alles darstellen und verwenden die Blockchain-Technologie, um den Besitz nachzuweisen.
+                <br/>
+                Manchmal sind sie mit einem digitalen Bild, GIF oder Lied verbunden. Manchmal können sie für ein physisches Objekt eingelöst werden. Manchmal hat der Besitzer Anspruch auf ein Erlebnis, wie ein Konzert oder ein Meet and Greet. PLANET NFTs stellen den Zugang zu realen und lebenslangen Erfahrungen, Musik, physischen Gegeständen, digitaler Kunst, virtuellen Abenteuern und mehr dar.</p>
+            </div>
+        </section>
+    )
+}
--- planet/src/index.js
+++ planet/src/index.js
@@ -4,6 +4,7 @@ import Footer from './Footer/Footer';
 import './index.css';
 import Navigation from './Navigation/Navigation';
 import StartseiteStart from './StartseiteStart/StartseiteStart';
+import WasNft from './WasNft/WasNft';
 import WasPlanetCamora from './WasPlanetCamora/WasPlanetCamora';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -12,6 +13,7 @@ root.render(
     <Navigation/>
     <StartseiteStart/>
     <WasPlanetCamora/>
+    <WasNft/>
     <Footer/>
   </React.StrictMode>
 );
