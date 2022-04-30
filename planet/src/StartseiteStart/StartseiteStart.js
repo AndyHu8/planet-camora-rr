91820fc3e503ff111b669a1b9894da70ac56786c
Andy Hu <huandy01@gmail.com>
2022-04-30 19:56:08 +0200

    WasNft + Smooth Scrolling

--- planet/src/StartseiteStart/StartseiteStart.js
+++ planet/src/StartseiteStart/StartseiteStart.js
@@ -12,7 +12,7 @@ export default function StartseiteStart() {
                     <p>Raf Camoras Album ZUKUNFT feiert den Gold Erfolg! Zum Dank an seine Fans, möchte Raf Camora mit dem Projekt PLANET CAMORA etwas zurückgeben. Erhalte jetzt einen zertifizierten Teil der Goldplatte des Raf Camora Albums Zukunft. Erhalte Zugriff zum ersten NFT Song release im deutschsprachigen Raum, sowie lifetime VIP Konzerttickets, exklusive Meet and Greets, Inner Circle Memberships und vieles mehr.</p>
                     <div className="allButtons">
                         <div className="button" onClick={() => window.open("https://www.planet-camora.com/", "_blank")}>Offizielle Seite</div>
-                        <div className="button">Was ist ein NFT?</div>
+                        <a href="#WasNftSection"><div className="button">Was ist ein NFT?</div></a>
                         <div className="button" onClick={() => window.open("https://hulongofficial.de/", "_blank")}>Created by</div>
                     </div>
                 </div>
--- planet/src/WasNft/WasNft.js
+++ planet/src/WasNft/WasNft.js
@@ -2,7 +2,7 @@ import "./WasNft.css"
 
 export default function WasNft() {
     return (
-        <section className="WasNftSection">
+        <section id="WasNftSection" className="WasNftSection">
             <div>
                 <h2>Was ist ein <span>NFT</span>?</h2>
                 <p>NFT steht für nicht fungible Token. NFTs sind digitale Unikate, welche nicht ersetzt werden können. Diese können so ziemlich alles darstellen und verwenden die Blockchain-Technologie, um den Besitz nachzuweisen.
