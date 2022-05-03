a19caab5fe744fa4dc9e05dc585702a63346198c
Andy Hu <huandy01@gmail.com>
2022-05-04 00:10:05 +0200

    .

---
+++ planet/src/VorteileSeite/VorteileSeite.js
@@ -0,0 +1,15 @@
+import '../index.css';
+import Faq from "../Faq/Faq";
+import Footer from "../Footer/Footer";
+import Navigation from "../Navigation/Navigation";
+
+export default function VorteileSeite(){
+    return (
+        <>
+        <Navigation/>
+        <Faq/>
+        <Footer/>
+        </>
+
+    )
+}
