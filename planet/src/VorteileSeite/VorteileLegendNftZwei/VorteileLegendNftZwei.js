787c5fa25414e5e92acd8c3f2e5e479c31e78e9b
Andy Hu <huandy01@gmail.com>
2022-05-11 20:21:16 +0200

    Fixes and new Folder

--- planet/src/VorteileSeite/VorteileLegendNftZwei/VorteileLegendNftZwei.js
+++ planet/src/VorteileSeite/VorteileLegendNftZwei/VorteileLegendNftZwei.js
@@ -4,7 +4,7 @@ export default function VorteileLegendNftZwei() {
     return (
         <div className="VorteileLegendNftZweiContainer">
             <div className="VorteileLegendNftZweiContent">
-            <h3>Vorteile als NFT Besitzer eines legendären NFTs</h3>
+            <h2>Vorteile als NFT Besitzer eines legendären NFTs</h2>
             <br/>
             <p>Im Grunde hat Dein NFT einen einzigartigen Wert. Wenn Du Dein NFT haltest, kann dieses je nach Nachfrage an Wert steigen und du hast zusätzlich die Möglichkeiten dieses am sekundär Markt auf Opensea weiterverkaufen. Wie das genau funktioniert, erfährst Du in unseren Video Anleitungen.</p>
             <p>Du kannst dein NFT entweder gegen seinen physichen Wert einlösen oder es auf dem sekundären Markt weitergverkaufen. Um dein NFT einzulösen schreibe unserem Support. Stelle sicher, dass Du deine Daten im Account richtig angegeben hast um den physischen Wert deines NFTs zu erhalten. Dies betrifft die Songnotizen, exklusiven Merch oder lifetime Konzert Zugang.</p>
